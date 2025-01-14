248c9cc94ceb2563aacce03249b1b5baebba05bf
xiaoxin <onlinedear@163.com>
2025-01-14 08:51:40 +0800

    Update new.tsx

--- src/components/modals/presets/new/new.tsx
+++ src/components/modals/presets/new/new.tsx
@@ -33,7 +33,7 @@ export function New() {
 
   return (
     <div className={styles.new}>
-      <h3 className={styles.title}>New Preset</h3>
+      <h3 className={styles.title}>新建预设</h3>
 
       <form
         className={cn(styles.form, noSelected && styles.disabled)}
@@ -41,18 +41,18 @@ export function New() {
       >
         <input
           disabled={noSelected}
-          placeholder="Preset's Name"
+          placeholder="预设名称"
           required
           type="text"
           value={name}
           onChange={e => setName(e.target.value)}
         />
-        <button disabled={noSelected}>Save</button>
+        <button disabled={noSelected}>保存</button>
       </form>
 
       {noSelected && (
         <p className={styles.noSelected}>
-          To make a preset, first select some sounds.
+          要创建预设，请首先选择一些声音。
         </p>
       )}
     </div>
